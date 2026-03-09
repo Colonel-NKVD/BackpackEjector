a10f95a4966f5e983236e6233d2bdcd5e716ce9b
Colonel-NKVD <115472499+Colonel-NKVD@users.noreply.github.com>
2026-03-09 19:07:19 +0700

    Create BackpackEjectorConfiguration.cs

---
+++ BackpackEjectorConfiguration.cs
@@ -0,0 +1,16 @@
+using Rocket.API;
+
+namespace BackpackEjector
+{
+    public class BackpackEjectorConfiguration : IRocketPluginConfiguration
+    {
+        public bool Enabled;
+        public float EjectForce;
+
+        public void LoadDefaults()
+        {
+            Enabled = true;
+            EjectForce = 10f;
+        }
+    }
+}
